10b6b2cde33237592560d48cefd2b29747a532a6
Petra Rábai <petra.rabai@icloud.com>
2022-12-03 13:12:47 +0100

    solve day 1 puzzle one part 1

---
+++ AdventOfCode2022/Day1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+	public class Day1
+	{
+		public int maxCalorie;
+		public int topThreeCaloriesTotal;
+		
+		public void Puzzle1()
+		{
+			int[] calories = ReadCalories();
+			int[] totalCaloriesPerElf = CountCaloriesPerElf(calories);
+
+			maxCalorie = SeekMaxCalorie(totalCaloriesPerElf);
+			
+		}
+
+		public void Puzzle2()
+		{
+			int[] calories = ReadCalories();
+			int[] totalCaloriesPerElf = CountCaloriesPerElf(calories);
+
+			topThreeCaloriesTotal = SeekTopTreeCaloriesAndSumIt(totalCaloriesPerElf);
+		}
+
+		private int[] ReadCalories()
+		{
+			string dataSource = "E:\\source\\AdventOfCode2022\\AdventOfCode2022\\PuzzlesInput\\Day1.txt";
+			string emptyLine = "";
+			
+			string[] data = File.ReadAllLines(dataSource);
+			int[] calorieData = new int[data.Length];
+
+			for (int i = 0; i < data.Length; i++)
+			{
+				if (data[i] != emptyLine)
+				{
+					calorieData[i] = Convert.ToInt32(data[i]);
+				}
+				else 
+				{
+					calorieData[i] = 0;
+				}
+			}   
+			
+			
+			return calorieData;
+		}
+
+		private int SeekMaxCalorie(int[] calories)
+		{
+			return calories.Max();
+		}
+
+		private int SeekTopTreeCaloriesAndSumIt(int[] calories)
+		{
+			int sum = 0;
+			calories.OrderByDescending(num => num, new SpecialComparer());
+			return sum;
+		}
+
+		private int[] CountCaloriesPerElf(int[] calories) 
+		{
+			int[] caloriesPerElf = new int[calories.Length];
+			int caloriePerElf = 0;
+			int count = 0;
+
+			foreach (int item in calories)
+			{
+				if (item != 0)
+				{
+					caloriePerElf += item;
+				}
+
+				if (item == 0)
+				{
+					caloriesPerElf[count] = caloriePerElf;
+					caloriePerElf = 0;
+					count++;
+				}
+					
+			}
+				
+			return caloriesPerElf;
+		}
+	}
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using AdventOfCode2022;
+
+Console.WriteLine("Advent Of Code 2022");
+Day1 Day1= new Day1();
+Day1.Puzzle1();
+Console.WriteLine("Day 1: \nPuzzle 1: \n");
+Console.WriteLine(Day1.maxCalorie);
